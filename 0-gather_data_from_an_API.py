ae41908438cca50e02ba4ab63140af53293b1ac5
Somed-1 <abbasovmaqa159@gmail.com>
2024-04-27 14:04:34 +0400

    Task 0

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""."""
+
+import requests
+import sys
+
+TASK_URL = "https://jsonplaceholder.typicode.com/todos"
+USER_URL = "https://jsonplaceholder.typicode.com/users/"
+
+
+def employee_info(user_id: str):
+    """."""
+    task_response = requests.get(TASK_URL + "?userId=" + user_id).json()
+    user_response = requests.get(USER_URL + user_id).json()
+
+    name = user_response.get("name")
+    total_tasks = len(task_response)
+    done_tasks = len([task for task in task_response
+                      if task.get("completed") is True])
+    title_tasks = [
+        task.get("title") for task in task_response
+        if task.get("completed") is True
+    ]
+
+    result = "Employee {} is done with tasks({}/{}):\n\t ".format(
+        name, done_tasks, total_tasks
+    )
+    result += "\n\t ".join(title_tasks)
+
+    return result
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: 0-gather_data_from_an_API.py <employee_id>")
+    user_id = sys.argv[1]
+    print(employee_info(user_id))
